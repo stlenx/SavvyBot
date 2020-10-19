b3539cc4e4145249ec52ccec0296804b3053b6fa
stlenx <69406526+stlenx@users.noreply.github.com>
2020-10-19 20:31:23 +0200

    Add files via upload

---
+++ BotDiscordTest/BotDiscordTest/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace SavvyBot
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Bot bot = new Bot();
+            bot.RunAsync().GetAwaiter().GetResult();
+        }
+    }
+}
